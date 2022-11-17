3d00bef12922901239b9d11bc89ee57d9ffddf7a
kikangseok <rlrkdtjr123@naver.com>
2022-11-17 20:18:49 +0900

    Chapter12_1117

---
+++ boj/단계별 풀어보기/12.브루트포스/1436.py
@@ -0,0 +1,11 @@
+import sys
+n = int(sys.stdin.readline())
+cnt=0
+num = 666
+while True:
+    if '666' in str(num):
+        cnt += 1 
+    if cnt == n:
+        print(num)
+        break
+    num += 1
