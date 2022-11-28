c71dcd1d37dbe2c2683ca78ef6260c4493a9a0df
kikangseok <rlrkdtjr123@naver.com>
2022-11-28 09:56:25 +0900

    Chap14_221128

--- boj/단계별 풀어보기/14.기하1/1085.py
+++ boj/단계별 풀어보기/14.기하1/1085.py
@@ -1,12 +1,3 @@
 import sys 
 x, y ,w, h = map(int,sys.stdin.readline().split())
-if x > w - x:
-    xDistance = w - x 
-else: 
-    xDistance = x 
-if y > h - y:
-    yDistance = h - y
-else: 
-    yDistance = y 
-     
-print(min(xDistance,yDistance))
+print(min(x,y,w-x,h-y))
