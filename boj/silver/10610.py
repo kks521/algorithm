f209d53c7600a1b50ec323c93700a5d59735d306
KangseokKi <rlrkdtjr123@naver.com>
2022-12-13 09:54:33 +0900

    Silver_greedy_221213

---
+++ boj/silver/10610.py
@@ -0,0 +1,16 @@
+#10610, 실버4, 그리디 알고리즘 
+
+import sys 
+n = sys.stdin.readline().rstrip()
+
+if '0' not in n:
+    print(-1)
+else: 
+    sum = 0 
+    for i in n:
+        sum += int(i)
+    if sum % 3 != 0:
+        print(-1)
+    else: 
+        n = sorted(n,reverse=True)
+        print(''.join(n))
