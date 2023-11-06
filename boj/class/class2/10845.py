1a6c09649c17f3201c3bb792425f12a4d981749f
KangseokKi <rlrkdtjr123@naver.com>
2023-11-06 21:01:42 +0900

    Class2_231106

---
+++ boj/class/class2/10845.py
@@ -0,0 +1,47 @@
+import sys 
+from collections import deque
+
+cnt = int(sys.stdin.readline())
+temp = deque()
+
+for _ in range(cnt):
+    order = sys.stdin.readline().rstrip().split()
+    if len(order) > 1:
+        order[1] == int(order[1])
+    
+    if order[0] == 'push':
+        temp.append(order[1])
+    
+    if order[0] == 'pop':
+        if temp:
+            print(temp.popleft())
+        else:
+            print(-1)
+
+    if order[0] == 'size':
+        print(len(temp))
+
+    if order[0] == 'empty':
+        if temp:
+            print(0)
+        else:
+            print(1)
+
+    if order[0] == 'front':
+        if temp:
+            print(temp[0])
+        else:
+            print(-1)
+
+    if order[0] == 'back':
+        if temp:
+            print(temp[1])
+        else:
+            print(-1)
+    
+
+
+            
+    
+
+    
