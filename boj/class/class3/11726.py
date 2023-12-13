190b37e33c7d1f3261723d0e7533e18171618f8f
kikangseok <rlrkdtjr123@naver.com>
2023-12-13 11:34:52 +0900

    Class3_dp_231213

---
+++ boj/class/class3/11726.py
@@ -0,0 +1,13 @@
+#11726, 실버3, dp
+import sys 
+
+n = int(sys.stdin.readline())
+arr = [0] * 1001
+
+arr[1] = 1 
+arr[2] = 2 
+
+for i in range(3,n+1):
+    arr[i] = (arr[i-2] + arr[i-1]) % 10007
+
+print(arr[n])
